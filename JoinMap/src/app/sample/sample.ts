cb01e90f401b6e9e81747b6fd1c0724ae69fc869
rahulprasad1399 <rahul.prasad@geekywolf.com>
2026-02-01 15:58:37 +0530

    maps

--- JoinMap/src/app/sample/sample.ts
+++ JoinMap/src/app/sample/sample.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { forkJoin, of, switchMap } from 'rxjs';
+import { concatMap, exhaustMap, forkJoin, from, map, mergeMap, of, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-sample',
@@ -47,15 +47,43 @@ export class Sample {
 
   // SWITCH MAP 
 
+  // http = inject(HttpClient);
+  // searchControl : FormControl = new FormControl()
+
+  // constructor() {
+  //   this.searchControl.valueChanges.pipe(
+  //     // Only the latest search term is processed, all previous pending requests are cancelled
+  //     switchMap((search)=>this.http.get('https://jsonplaceholder.typicode.com/posts?id='+search))
+  //   ).subscribe({
+  //     next : (val) => console.log(val)
+  //   })
+  // }
+
+  // MERGE MAP 
+  // constructor() {
+  //   from([1, 2, 3]).pipe(
+  //     mergeMap(num => of(num*10))
+  //   )
+  //   .subscribe({
+  //     next : (res) => console.log(res),
+  //     error : (err) => console.log(err)
+  //   })
+  // }
+  // Output
+  // 10
+  // 20
+  // 30
+
   http = inject(HttpClient);
   searchControl : FormControl = new FormControl()
 
   constructor() {
     this.searchControl.valueChanges.pipe(
       // Only the latest search term is processed, all previous pending requests are cancelled
-      switchMap((search)=>this.http.get('https://jsonplaceholder.typicode.com/posts?id='+search))
+      exhaustMap((num)=>this.http.get('https://jsonplaceholder.typicode.com/posts?id='+num))
     ).subscribe({
       next : (val) => console.log(val)
     })
   }
+
 }
